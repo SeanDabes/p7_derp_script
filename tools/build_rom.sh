f843b95df52b394e5f174d18da3f92480d128afd
Sean Dabes <dabessean@gmail.com>
2026-03-12 22:13:13 +0100

    Process optimization - Uncompress target-files directly from its location - Take images from target files

--- tools/build_rom.sh
+++ tools/build_rom.sh
@@ -74,23 +74,36 @@ builddevice() {
 
 }
 
-# 1. Make target-files package in user variant and place it in a safe place
+# 1. Make target-files package in user variant
 info="clear; echo; echo -e '${WHITEONMAGENTA} Building ROM with $3 simultaneous processes... ${NOCOLOR}'; echo; bash -c '$(declare -f wait_task); wait_task'"
 command="clear; . build/envsetup.sh && lunch lineage_$1-$android_version-user && m target-files-package -j $3; touch /tmp/end_task"
 
 builddevice $1
 
-mv "$src_dir""obj/PACKAGING/target_files_intermediates/$target_files_zip" $out_dir
-files=(boot.img dtbo.img init_boot.img vendor_kernel_boot.img) # also the rest of images
+# 2. Uncompress target-files in separate directory
+echo "- Uncompressing target_files..."
+unzip -q "$src_dir""obj/PACKAGING/target_files_intermediates/$target_files_zip" -d $work_dir
+sleep 2
+
+# 3. Take images
+echo "- Copying images..."
+files=(boot.img dtbo.img init_boot.img vendor_kernel_boot.img)
 for item in "${files[@]}"; do
-    if [ -f $src_dir$item ]; then
-        mv $src_dir$item $out_dir
+    echo -n "$item..."
+    if [ -f "$work_dir/IMAGES/$item" ]; then
+        cp "$work_dir/IMAGES/$item" $out_dir
+        if [ -f "$out_dir""/$item" ]; then
+            echo -e "${GREEN}OK${NOCOLOR}"
+        else
+            echo -e "${RED}ERROR${NOCOLOR}"
+        fi
     else
         ERROR=true
     fi
 done
+sleep 2
 
-# 2. Make recovery in userdebug variant and place it at the same place
+# 4.  Make recovery in userdebug variant and place it at the same place
 info="clear; echo; echo -e '${WHITEONMAGENTA} Building RECOVERY with $3 simultaneous processes... ${NOCOLOR}'; echo; bash -c '$(declare -f wait_task); wait_task'"
 command="clear; . build/envsetup.sh && lunch lineage_$1-$android_version-userdebug &&  m vendorbootimage -j $3; touch /tmp/end_task"
 
@@ -98,13 +111,8 @@ builddevice $1
 
 mv "$src_dir""vendor_boot.img" "$out_dir"
 
-# 3. Uncompress target-files
+# 5. Replace vendor_boot in user variant by the userdebug variant
 cd $out_dir
-echo -n "- Uncompressing target_files..."
-unzip -q "$target_files_zip" -d "$work_dir"
-echo -e "${GREEN}OK${NOCOLOR}"
-
-# 4. Replace vendor_boot in user variant by the userdebug variant
 echo -n "- Replacing vendor_boot by userdebug version..."
 cp "vendor_boot.img" "$work_dir/IMAGES/vendor_boot.img"
 if [ -f "$work_dir/IMAGES/vendor_boot.img" ]; then
@@ -114,7 +122,7 @@ else
     ERROR=true
 fi
 
-# 5. Make zip again with the new vendor_boot
+# 6. Make zip again with the new vendor_boot
 echo -n "- Compressing files..."
 cd "$work_dir"
 zip -q -r -y -X -0 "target_files_mod.zip" .
@@ -125,15 +133,14 @@ else
     ERROR=true
 fi
 
-# 6. Make ota package with the modified files
+# 7. Make ota package with the modified files
 info="clear; echo; echo -e '${WHITEONMAGENTA} Building OTA Package... ${NOCOLOR}'; echo; bash -c '$(declare -f wait_task); wait_task'"
 command="clear; . build/envsetup.sh && lunch lineage_$1-$android_version-userdebug && ota_from_target_files $work_dir/target_files_mod.zip $out_dir/DerpFest-v$derp_branch-$start_date-$1-Official-Beta.zip; touch /tmp/end_task"
 
 builddevice $1
 
-# 7. Clean
+# 8. Clean
 rm -rf $work_dir
-rm "$out_dir/$target_files_zip"
 
 sleep 5
 echo
